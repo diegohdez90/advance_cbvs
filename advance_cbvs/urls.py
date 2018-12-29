c95de065f5518cd924035dd261dfbfed71b01fea
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-28 18:46:50 -0600

    refactor: update index view with cbv class

--- advance_cbvs/urls.py
+++ advance_cbvs/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from basic_app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.CBView.as_view()),
 ]
