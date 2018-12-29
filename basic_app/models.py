aa4cb7e0b75fb726dbf661fc0ffd9e77eb115d68
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-28 19:23:27 -0600

    feat: add school and student model into app and admin settings

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+  name = models.CharField(max_length=128)
+  principal = models.CharField(max_length=80)
+  location = models.CharField(max_length=64)
+
+  def __str__(self):
+    return self.name
+
+class Student(models.Model):
+  name = models.CharField(max_length=80)
+  age = models.PositiveIntegerField()
+  school = models.ForeignKey(School, related_name='students')
+
+  def __str__(self):
+    return self.name
