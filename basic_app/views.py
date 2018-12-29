f133a5db553d285d6eb8b360089a4ce79f6ffd8a
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-12-28 19:30:51 -0600

    feat: init templates in app dir
    init with ListView and Detail classes

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 
+class SchollListView(ListView):
+  model = models.School
 
-class IndexView(TemplateView):
-  template_name = 'index.html'
-
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['injectme'] = "Injection in CBV's"
-    return context;
+class SchoolDetailView(DetailView):
+  model = models.School
+  template_name = 'basic_app/school_detail.html'
